a8bd1b02f9b2fb8b792a15d01dae0194eb066f64
alexiaCat <alexia_12ceci@hotmail.com>
2023-10-02 04:13:02 -0300

    [add buttons]

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,58 @@
+import Pentagon from '/images/bg-pentagon.svg';
+import Triangle from '/images/bg-triangle.svg';
+import Logo from '/images/logo.svg';
+import Piedra from '/images/icon-rock.svg';
+import Papel from '/images/icon-paper.svg';
+import Tijera from '/images/icon-scissors.svg';
+
 
 function App() {
 
 
   return (
     <>
-    <div className="bg-radial-gradient w-screen h-screen">
-         <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-   
+      <div className="font-barlow-semi-condensed text-white flex flex-col items-center">
+        <div className="flex py-6 px-7 flex-row justify-between w-[50%] h-[170px]  border-[3px] border-neutral-header-outline rounded-2xl mt-14">
+          <img
+            src={Logo}
+            className='w-[25%]'
+          />
+          <div className="bg-white text-neutral-score-text min-h-max rounded-lg w-[170px] text-[16px] text-center">
+            <h4 className='mt-2'>SCORE</h4>
+            <h1 className="text-neutral-dark-text font-bold text-[70px]">12</h1>
+          </div>
+        </div>
+        {/* container buttons */}
+        <div className="mt-[150px] relative">
+        <img
+            src={Triangle}
+            className='absolute left-[50%]  translate-x-[-50%] z-0'
+          />
+          <button className='bg-white w-[220px] h-[220px] shadow1 border-primary-paper-gradient-start border-[25px] rounded-full absolute z-10'>
+            <img
+              src={Papel}
+              className='mx-auto w-[50%]'
+            />
+          </button>
+
+          <button className='bg-white w-[220px] h-[220px] shadow3 border-primary-scissors-gradient-start border-[25px] rounded-full absolute   z-10'>
+            <img
+              src={Tijera}
+              className='mx-auto w-[50%]'
+            />
+          </button>
+          <button className='bg-white w-[220px] h-[220px] shadow2 border-primary-rock-gradient-start border-[25px] rounded-full absolute z-10 left-50 translate-x-[-50%]'>
+            <img
+              src={Piedra}
+              className='mx-auto w-[50%]'
+            />
+          </button>
+     
+      
+        </div>
+
+      </div>
+
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'radial-gradient': 'radial-gradient(circle, #1f3756 50%, #141539)',
+        'radial-gradient': 'radial-gradient(circle at top center, #1f3756 50%, #141539)',
       },
       colors: {
         primary: {
@@ -17,8 +17,6 @@ export default {
           'paper-gradient-end': 'hsl(230, 89%, 65%)',
           'rock-gradient-start': 'hsl(349, 71%, 52%)',
           'rock-gradient-end': 'hsl(349, 70%, 56%)',
-          'lizard-gradient-start': 'hsl(261, 73%, 60%)',
-          'lizard-gradient-end': 'hsl(261, 72%, 63%)',
           'cyan-start': 'hsl(189, 59%, 53%)',
           'cyan-end': 'hsl(189, 58%, 57%)',
         },
@@ -27,6 +25,9 @@ export default {
           'score-text': 'hsl(229, 64%, 46%)',
           'header-outline': 'hsl(217, 16%, 45%)',
         },
+      },
+      fontFamily: {
+        'barlow-semi-condensed': ['"Barlow Semi Condensed"', 'sans'],
       },
     },
   },
